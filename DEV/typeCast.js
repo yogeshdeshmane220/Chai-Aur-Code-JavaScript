eb4ce3777797da9596ca6c1fc94eb725cb0c6f60
Yogesh Deshmane <92244185+yogeshdeshmane220@users.noreply.github.com>
2023-11-27 04:09:14 +0000

    Changed into The Veriables

---
+++ DEV/typeCast.js
@@ -0,0 +1,8 @@
+let num = "33";
+console.log(typeof (num))
+// If Your Wanting To COnvert the number into the String 
+// Then You Have to use the value cast according 
+
+
+let newNum = Number(num);//This line is converting the String into Number
+ console.log(typeof newNum)
---
+++ DEV/veriables.js
@@ -0,0 +1,14 @@
+// "use strict"
+// let account = "Instagram";
+// console.log(account);
+
+// Preffered type To cast dataTypes
+let a = "2" + 3;
+console.log(a);
+console.log(typeof a); //SO Here It converts the Data 2 and 3 into the string
+let b = 2 + "3";
+console.log(b);
+console.log(typeof b); //SO Here It converts the Data 2 and 3 into the string
+
+let c = 1 + 2 + "3";
+console.log(c);
